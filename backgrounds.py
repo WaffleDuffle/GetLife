2e67b8a66d696b1c4a83eae51cfa38bbcc1da772
waffleduffle <denis.io.constantin@gmail.com>
2023-12-26 01:23:47 +0200

    added doctor list, finally

--- backgrounds.py
+++ backgrounds.py
@@ -3,3 +3,4 @@ bg_login_image_path = '/home/waffleduffle/Desktop/python_proj_etti/GetLife/resou
 
 bg_main_menu_image_path = '/home/waffleduffle/Desktop/python_proj_etti/GetLife/resources/background_main_menu_python_project_etti.png'
 
+bg_doc_menu_image_path = '/home/waffleduffle/Desktop/python_proj_etti/GetLife/resources/background_without_logo.png'
--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import backgrounds as back
 import mysql.connector
 from tkinter import PhotoImage
 from dotenv import load_dotenv
-
-
+from PIL import Image, ImageTk
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        # name of the app
+        #name of the app
         self.master.title("GetLife")
 
         #load canvas
@@ -25,7 +24,7 @@ class Application(tk.Frame):
         
         #refresh page
         self.refresh(back.bg_login_image_path)
-       # self.canvas.create_text(500, 700, anchor=tk.S,text='  Constantin Denis\nMoroiu Eric-Gabriel', fill='black')
+        #self.canvas.create_text(500, 700, anchor=tk.S,text='  Constantin Denis\nMoroiu Eric-Gabriel', fill='black')
     
         #create blank character for positioning
         self.blank = tk.Label(self.canvas, text='', bg='#cf71ff')
@@ -52,7 +51,7 @@ class Application(tk.Frame):
 
     def check_cred(self, name1, name2):
         load_dotenv()
-        # Delete the previous text item if it exists, hasattr check if given text exists before attempting to delete it
+        #delete the previous text item if it exists, hasattr checks if given text exists before attempting to delete it
         if hasattr(self.canvas, 'text') and self.canvas.text is not None:
             self.canvas.delete(self.canvas.text)
 
@@ -71,15 +70,15 @@ class Application(tk.Frame):
         self.blank2 = tk.Label(self.canvas, text='', bg='black')
         self.blank2.pack(pady = 150)
 
-        #Show Doctors
+        #show Doctors
         self.print_doctors = tk.Button(self.canvas, text="Doctors", padx=50, pady=10, command=lambda: self.doctors_menu())
         self.print_doctors.pack(side="top", pady= 15)
 
-        #Scheduling button
+        #scheduling button
         self.schedule_button = tk.Button(self.canvas, text="Schedule a meeting", padx=50, pady=10, command=lambda: print('ok meeting!'))
         self.schedule_button.pack(pady= 15)
 
-        #Exit button
+        #exit button
         self.quit = tk.Button(self.canvas, text="EXIT", padx=50, fg="red", command=root.destroy)
         self.quit.pack(side='bottom', pady=15)
 
@@ -89,18 +88,85 @@ class Application(tk.Frame):
 
 
     def doctors_menu(self):
+        #refresh page
+        self.refresh(back.bg_doc_menu_image_path)
+        
+        #initialize cursor in database
         self.db_cursor = conn.cursor() 
         self.db_cursor.execute("SELECT * FROM Medici")
         self.db_result = self.db_cursor.fetchall()
-        for i in range(len(self.db_result)):
-            for element in self.db_result[i]:
-                print(element)
+        self.i = 0
+        
+
+        #buttons for crossing 
+        self.next_button = tk.Button(self.canvas, padx = 28, text='Next', command=lambda: self.next_doc()).pack(side=tk.RIGHT, anchor=tk.CENTER, padx=20)
+        self.previous_button = tk.Button(self.canvas, text='Previous', command=lambda: self.previous_doc()).pack(side=tk.LEFT,padx=20)
+        
+        #return button
+        self.ret = tk.Button(self.canvas, text="Return", padx=50, command=lambda:self.main_menu())
+        self.ret.pack(side='bottom', pady=30)
+
+        #print first medic
+        self.print_medici()
+
+    def next_doc(self):
+        
+        self.i += 1
+        if self.i == len(self.db_result) or self.i < 0:
+            print('OUT OF BOUNDS!!!')
+            self.i = 0    
+        self.print_medici()
+        
+
+    def previous_doc(self):
+        self.i -= 1
+        if self.i == len(self.db_result) or self.i < 0:
+            print('OUT OF BOUNDS!!!')
+            self.i = len(self.db_result) - 1            
+        self.print_medici()
+        
+        
+    def print_medici(self):
+        #checks if image_label has been defined already
+        if hasattr(self, 'image_label'):
+            self.image_label.pack_forget()
+            self.medic_ID.pack_forget()
+            self.medic_nume.pack_forget()
+            self.medic_specialitate.pack_forget()
+            self.medic_disponibilitate.pack_forget()
+            self.medic_contact.pack_forget()
+
+        self.image_path = '/home/waffleduffle/Desktop/python_proj_etti/GetLife/resources/' + str(self.i) + '.png'
+        self.image = Image.open(self.image_path)
+        self.photo = ImageTk.PhotoImage(self.image)
+
+        #create label for image
+        self.image_label = tk.Label(self.canvas, image=self.photo)
+        self.image_label.pack(pady=50)
+
+        #label for ID
+        self.medic_ID = tk.Label(self.canvas, text = 'ID: ' + str(self.db_result[self.i][0]))
+        self.medic_ID.pack(pady=5)
+        
+        #label for name
+        self.medic_nume = tk.Label(self.canvas, text = 'Nume: ' + str(self.db_result[self.i][1]))
+        self.medic_nume.pack(pady=5)
+
+        
+        self.medic_specialitate = tk.Label(self.canvas, text = 'Specialitate: ' + str(self.db_result[self.i][2]))
+        self.medic_specialitate.pack(pady=5)
+
+        self.medic_disponibilitate = tk.Label(self.canvas, text = 'Disponibilitate: ' + str(self.db_result[self.i][3]))
+        self.medic_disponibilitate.pack(pady=5)
+
+        self.medic_contact = tk.Label(self.canvas, text = 'Contact: ' + str(self.db_result[self.i][4]))
+        self.medic_contact.pack(pady=5)
+
 
     def refresh(self, background_name):
         #delete widgets
         for widget in self.canvas.winfo_children():
             widget.destroy()
-
         #import background
         self.canvas.delete("all")
         self.bg_image = PhotoImage(file=background_name)
@@ -108,14 +174,15 @@ class Application(tk.Frame):
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.bg_image)
 
 #main loop
+        
 if __name__ == '__main__':
     root = tk.Tk()
     conn = mysql.connector.connect(
         host='localhost',
-        password='MySQL1234', 
-        user='root', 
-        database='MyDB'
-    )
+	password='MySQL1234',
+	user='root',
+	database='MyDB'
+    )    
 
 if conn.is_connected():
     print('Connection established...')
